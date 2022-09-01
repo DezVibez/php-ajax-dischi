7049f39e80c8dcc9cbba0ffb068342e1f70c1be2
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-01 16:02:26 +0200

    push per ticket

--- seconda versione/script.js
+++ seconda versione/script.js
@@ -1,4 +1,13 @@
-<script>
+Vue.config.devtools = true;
 
-    
-</script>
+const app = new Vue({
+ el: '#root',
+ data: {
+    dischi: [],
+ },
+ mounted(){
+    axios.get('http://localhost:8888/api/').then((res)=>{
+    this.dischi = res.data
+    });
+ }
+});
