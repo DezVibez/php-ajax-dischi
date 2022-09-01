fe2a37e8e6ba3781549fd53894c4d221b44234ec
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-01 14:48:12 +0200

    proviamo se pusho perche bho

--- prima versione/index.php
+++ prima versione/index.php
@@ -1,8 +1,4 @@
-<?php 
-
-include __DIR__ . '../data/data.php';
-
-?>
+<?php include __DIR__. '../includes/data/data.php' ; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +6,7 @@ include __DIR__ . '../data/data.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prima Versione</title>
 
     <link rel="stylesheet" href="../css/style.css">
 </head>
